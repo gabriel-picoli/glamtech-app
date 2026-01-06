87c3e8f6c50088f802297e007d35fef4f0a6e468
gabriel-picoli <vnvsgabe@gmail.com>
2026-01-06 11:32:45 -0300

    feat(provider): create app provider and query configs

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,9 +9,7 @@ import {
   useFonts
 } from '@expo-google-fonts/inter'
 
-import { ThemeProvider } from 'styled-components/native'
-
-import theme from '../styles/theme'
+import AppProviders from '@/providers/AppProviders'
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -26,13 +24,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
+    <AppProviders>
       <StatusBar style="dark" />
 
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(private)" />
         <Stack.Screen name="(public)" />
       </Stack>
-    </ThemeProvider>
+    </AppProviders>
   )
 }
